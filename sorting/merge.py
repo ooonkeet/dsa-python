e65ecb0be257822446a0599d3475c4b0c630f1ec
Ankit <ankitspare981@gmail.com>
2026-09-12 01:07:43 +0530

    merge algorithm

---
+++ sorting/merge.py
@@ -0,0 +1,36 @@
+def merge(arr):
+    if len(arr)>1:
+        mid=len(arr)//2
+        L=arr[:mid]
+        R=arr[mid:]
+
+        merge(L)
+        merge(R)
+
+        i=j=k=0
+
+        while i<len(L) and j<len(R):
+            if L[i]<R[j]:
+                arr[k]=L[i]
+                i+=1
+            else:
+                arr[k]=R[j]
+                j+=1
+            k+=1
+
+        while i<len(L):
+            arr[k]=L[i]
+            i+=1
+            k+=1
+
+        while j<len(R):
+            arr[k]=R[j]
+            j+=1
+            k+=1
+
+n=int(input("Enter length of array: "))
+arr=[]*n
+for i in range(n):
+    arr.append(int(input("Enter element: ")))
+merge(arr)
+print("Sorted array is: ",arr)
